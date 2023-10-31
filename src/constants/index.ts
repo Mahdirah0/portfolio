8761a3871158552273ec0a993b40b04e5be355e8
Mahdirah0 <rahmanmahdi15@gmail.com>
2023-10-31 15:56:36 +0000

    feature: added resume pdf

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ export { PROJECT_DATA } from './projectData';
 export const GITHUB_PROFILE_URL = 'https://github.com/Mahdirah0';
 export const LINKED_IN_PROFILE_URL =
   'https://www.linkedin.com/in/mahdi-rahman-14383420a/';
-export const RESUME_URL = 'https://mahdirah0-resume.netlify.app/';
+export const RESUME_URL = 'https://mahdirah0-resume.netlify.app/resume.pdf';
 export const SKILLS = [
   'HTML5',
   'CSS3',
