0c534f5bf44804b4589b8b3d8963190fa40ff1f5
Mahdirah0 <rahmanmahdi15@gmail.com>
2024-04-22 15:08:29 +0100

    updated about section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
-import { LINKED_IN_PROFILE_URL } from '../constants';
+import { LINKED_IN_PROFILE_URL, RESUME_PATH } from '../constants';
 
 import { useState, useEffect } from 'react';
 import { HamburgerMenu } from './HamburgerMenu';
@@ -36,30 +36,35 @@ export const Header = () => {
   return (
     <header id='header' className='bg-neutral text-white'>
       <div className='flex w-4/5 m-auto items-center justify-between h-24'>
-        <h1 className='text-2xl'>
-          <Link to='/'>Mahdi Rahman</Link>
+        <h1 className='text-xl'>
+          <Link to='/'>MAHDI RAHMAN</Link>
         </h1>
         <div className='flex space-x-5'>
           {width < 635 ? (
             <HamburgerMenu />
           ) : (
             <nav>
-              <ul className='flex'>
-                <li className='mr-5'>
+              <ul className='flex gap-x-5'>
+                <li>
                   <Link to='/'>Home</Link>
                 </li>
-                <li className='mr-5'>
+                <li>
                   <HashLink smooth to={'/#about'}>
                     About
                   </HashLink>
                 </li>
-                <li className='mr-5'>
+                <li>
                   <HashLink smooth to={'/#projects'}>
                     Projects
                   </HashLink>
                 </li>
                 <li>
-                  <Link className='' to={LINKED_IN_PROFILE_URL} target='_blank'>
+                  <Link to={RESUME_PATH} target='_blank'>
+                    Resume
+                  </Link>
+                </li>
+                <li>
+                  <Link to={LINKED_IN_PROFILE_URL} target='_blank'>
                     Contact Me
                   </Link>
                 </li>
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,18 +1,18 @@
 export const About = () => {
   return (
     <div className='bg-neutral-100'>
-      <div
-        id='about'
-        className='flex flex-col justify-center items-center m-auto py-32'
-      >
-        <h1 className='mb-5 text-4xl font-bold'>About Me</h1>
-        <p className='mx-4 prose lg:prose-xl text-center'>
-          Passionate software developer skilled in JavaScript, Python, React,
-          and TypeScript. Strong problem-solving abilities and eager to turn
-          ideas into functional applications. Committed to continuous learning
-          and collaborative teamwork. Excited to make a meaningful impact in
-          software development.
-        </p>
+      <div id='about' className='flex justify-center py-32'>
+        <div>
+          <h1 className='mb-5 text-4xl font-bold'>About Me</h1>
+          <p className='prose lg:prose-xl'>
+            Passionate software developer skilled in JavaScript, Python, React,
+            and TypeScript. Strong problem-solving abilities and eager to turn
+            ideas into functional applications. Committed to continuous learning
+            and collaborative teamwork. Excited to make a meaningful impact in
+            software development.
+          </p>
+          <p className='prose lg:prose-xl'>Email: mrahman2525@outlook.com</p>
+        </div>
       </div>
     </div>
   );
